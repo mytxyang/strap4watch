ada1fa278d663d0cc95436435210da39b2f1519c
lincai <lincai@cn.ibm.com>
2018-12-21 16:36:46 +0800

    auto play

--- main.js
+++ main.js
@@ -19,6 +19,18 @@ function loadStrapImage(imgName) {
 }
 
 var straps;
+var timerID;
+var watch_str = "50fathoms.png constellation.png khaki.png pp5119.png sna411.png alange.png "
+    + "datejust.png	 legenddiver.png		railmaster.png		speedmaster.png "
+    + "ap25589ba.png		daytona.png		navitimer.png		rolex116710ln.png	speedytuesday.png "
+    + "autavia.png elprimero.png newman.png		rolex1803.png		sub.png "
+    + "avenger.png		explorer.png		nomos.png		rootbeer.png		tudor79090.png "
+    + "blackbay.png		fatlady.png		omega_tresor.png	sarb017.png		vc.png "
+    + "captaincook.png		flieger.png		oris65.png		sbdx021.png "
+    + "chronoris.png		gsgmt.png		panerai.png		seamaster.png "
+    + "ck2998.png		hulk.png		pelagos.png		sinn104.png "
+    + "cocktail.png		iwc.png			pp.png			skx007.png";   
+
 $( document ).ready(function() {
     console.debug("ready");
     $.getJSON( "straps.json", function( data ) {
@@ -29,5 +41,22 @@ $( document ).ready(function() {
     });
     $(".strapbtn").click(function() {
         loadStrapImage(this.getAttribute('src'));
+    });
+    $("#playbtn").click(function() {
+        if ($("span", this).text()==="STOP") {
+            $("span", this).text("PLAY ALL");
+            clearInterval(timerID);
+        } else {
+            $("span", this).text("STOP");
+
+            timerID = setInterval(function() {
+                var watches = watch_str.split(/[\s,;\t\n]+/);
+                var i = Math.floor((Math.random() * straps.length));
+                var j = Math.floor((Math.random() * watches.length));
+                $(".watchface").attr("src", "watch/"+watches[j].trim());
+                $(".strapup").attr("src", straps[i].file).delay(1000).fadeIn(300);
+                $(".strapdown").attr("src", straps[i].file.replace("_up", "_down")).delay(1000).fadeIn(300);
+            },  3000); // 60 * 1000 milsec
+        }
     });
 });
